3fbe73a7f48a863a6d14d066e910dd3716cfa308
Godfrey Antomarlin <godfrey.antomarlin@gmail.com>
2026-03-25 16:45:55 +0000

    Matcher: created process_payload structure

---
+++ src/core/Matcher.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "OrderBook.hpp"
+#include "Order.hpp"
+
+class Matcher {
+public:
+    void process_payload(OrderPayload payload) {
+        if (payload.action == ActionType::Cancel) {
+            book.remove_order(payload.order_id, payload.is_buy);
+            return;
+        } 
+
+        while (payload.quantity > 0) {
+            if (payload.is_buy) {
+                uint32_t best_ask = book.get_lowest_ask();
+                if (best_ask == NULL_NODE || best_ask > payload.price) {
+                    break;
+                }
+                // TODO: Execute Trade
+            } else {
+                uint32_t best_bid = book.get_highest_bid();
+                if (best_bid == NULL_NODE || best_bid < payload.price) {
+                    break;
+                }
+                // TODO: Execute Trade
+            }
+        }
+
+        if (payload.quantity > 0) {
+            book.add_order(payload.order_id, payload.price, payload.quantity, payload.is_buy);
+        }
+    }
+
+private:
+    OrderBook<1000000, 100000> book;
+
+};
