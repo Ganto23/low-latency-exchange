ed285d708abf07012360f3b952247a5cc627333d
Godfrey Antomarlin <godfrey.antomarlin@gmail.com>
2026-03-25 15:05:22 +0000

    OrderBook: Added add_order()

--- src/core/OrderBook.hpp
+++ src/core/OrderBook.hpp
@@ -14,7 +14,31 @@ public:
         asks.resize(MaxPrice);
     }
 
-    add_order()
+    void add_order(uint64_t order_id, uint32_t price, uint32_t quantity, bool is_buy) {
+        uint32_t new_idx = free_list.allocate();
+        orders[new_idx] = {order_id, price, quantity, NULL_NODE, NULL_NODE}; 
+        id_map[order_id] = new_idx;
+
+        PriceLevel& level = is_buy ? bids[price] : asks[price];
+        Bitboard& bits = is_buy ? bids_bits : asks_bits;
+
+        if (level.head_idx == NULL_NODE) {
+            level.head_idx = new_idx;
+            level.tail_idx = new_idx;
+            bits.set_active(price);
+        } else {
+            uint32_t old_tail = level.tail_idx;
+            orders[old_tail].next_idx = new_idx;
+            orders[new_idx].prev_idx = old_tail;
+            level.tail_idx = new_idx;
+        }
+
+        level.total_volume += quantity;
+    }
+
+    void remove_order(uint64_t order_id, bool is_buy) {
+        
+    }
 
 private:
     std::vector<OrderNode> orders;
